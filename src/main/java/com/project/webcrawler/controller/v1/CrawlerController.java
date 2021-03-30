edd7cdab519982ac0e84899174aef5b0bdde05d8
Davi Queiroz <daviqueiroz92@gmail.com>
2021-03-30 00:46:13 -0300

    Renomeando Método no controller e melhorando doc Swagger

--- src/main/java/com/project/webcrawler/controller/v1/CrawlerController.java
+++ src/main/java/com/project/webcrawler/controller/v1/CrawlerController.java
@@ -3,7 +3,11 @@ package com.project.webcrawler.controller.v1;
 import com.project.webcrawler.model.dto.CrawlerResponseDTO;
 import com.project.webcrawler.service.CrawlerService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +25,13 @@ public class CrawlerController {
     }
 
     @GetMapping
-    public CrawlerResponseDTO callCrawler() throws Exception {
+    @ApiOperation("InitWebCrawler")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK."),
+            @ApiResponse(code = 500, message = "Internal Server Error.", response = ResponseEntity.class),
+            @ApiResponse(code = 400, message = "Bad Request.")
+    })
+    public CrawlerResponseDTO initCrawler() throws Exception {
 
         return service.initCrawler();
     }
